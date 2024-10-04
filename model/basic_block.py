c320dd360c9b252bde4d98fb9cd4defea048c5b5
xiaotong-233 <64418031+xiaotong-233@users.noreply.github.com>
2024-10-04 12:33:43 +0800

    Update basic_block.py

--- model/basic_block.py
+++ model/basic_block.py
@@ -26,26 +26,3 @@ class LinearBlock(nn.Module):
         return x
 
 
-
-
-
-# class LinearBlock(nn.Module):
-#     def __init__(self, in_features, out_features=None, drop=0.0):
-#         super().__init__()
-#         out_features = out_features or in_features
-#         self.fc = nn.Linear(in_features, out_features)
-#         self.bn = nn.BatchNorm2d(out_features)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.drop = nn.Dropout(drop)
-#         self.fc.weight.data.normal_(0, math.sqrt(2. / out_features))
-#         self.bn.weight.data.fill_(1)
-#         self.bn.bias.data.zero_()
-#
-#     def forward(self, x):
-#         x = self.drop(x)
-#         x = self.fc(x)
-#         x = x.unsqueeze(2).unsqueeze(3)  # 添加两个维度以匹配BatchNorm2d的输入要求
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = x.squeeze(3).squeeze(2)  # 移除添加的维度
-#         return x
